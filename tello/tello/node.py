f738760f73faeff24061bcdc80f259c098e35d46
Håvard Pedersen Brandal <haavard.brandal@gmail.com>
2024-11-20 17:43:38 +0100

    Moved two functions to util.py

--- tello/tello/node.py
+++ tello/tello/node.py
@@ -23,6 +23,8 @@ from nav_msgs.msg import Odometry
 from cv_bridge import CvBridge
 import ament_index_python
 
+from tello.util import euler_to_quaternion, quaternion_to_euler
+
 # Tello ROS node class, inherits from the Tello controller object.
 #
 # Can be configured to be used by multiple drones, publishes, all data collected from the drone and provides control using ROS messages.
@@ -316,29 +318,6 @@ class TelloNode():
     def cb_flip(self, msg):
         self.tello.flip(msg.data)
 
-# Convert a rotation from euler to quaternion.
-def euler_to_quaternion(r):
-    (yaw, pitch, roll) = (r[0], r[1], r[2])
-    qx = math.sin(roll/2) * math.cos(pitch/2) * math.cos(yaw/2) - math.cos(roll/2) * math.sin(pitch/2) * math.sin(yaw/2)
-    qy = math.cos(roll/2) * math.sin(pitch/2) * math.cos(yaw/2) + math.sin(roll/2) * math.cos(pitch/2) * math.sin(yaw/2)
-    qz = math.cos(roll/2) * math.cos(pitch/2) * math.sin(yaw/2) - math.sin(roll/2) * math.sin(pitch/2) * math.cos(yaw/2)
-    qw = math.cos(roll/2) * math.cos(pitch/2) * math.cos(yaw/2) + math.sin(roll/2) * math.sin(pitch/2) * math.sin(yaw/2)
-    return [qx, qy, qz, qw]
-
-# Convert rotation from quaternion to euler.
-def quaternion_to_euler(q):
-    (x, y, z, w) = (q[0], q[1], q[2], q[3])
-    t0 = +2.0 * (w * x + y * z)
-    t1 = +1.0 - 2.0 * (x * x + y * y)
-    roll = math.atan2(t0, t1)
-    t2 = +2.0 * (w * y - z * x)
-    t2 = +1.0 if t2 > +1.0 else t2
-    t2 = -1.0 if t2 < -1.0 else t2
-    pitch = math.asin(t2)
-    t3 = +2.0 * (w * z + x * y)
-    t4 = +1.0 - 2.0 * (y * y + z * z)
-    yaw = math.atan2(t3, t4)
-    return [yaw, pitch, roll]
 
 def main(args=None):
     rclpy.init(args=args)
---
+++ tello/tello/util.py
@@ -0,0 +1,25 @@
+import math
+
+# Convert a rotation from euler to quaternion.
+def euler_to_quaternion(r):
+    (yaw, pitch, roll) = (r[0], r[1], r[2])
+    qx = math.sin(roll/2) * math.cos(pitch/2) * math.cos(yaw/2) - math.cos(roll/2) * math.sin(pitch/2) * math.sin(yaw/2)
+    qy = math.cos(roll/2) * math.sin(pitch/2) * math.cos(yaw/2) + math.sin(roll/2) * math.cos(pitch/2) * math.sin(yaw/2)
+    qz = math.cos(roll/2) * math.cos(pitch/2) * math.sin(yaw/2) - math.sin(roll/2) * math.sin(pitch/2) * math.cos(yaw/2)
+    qw = math.cos(roll/2) * math.cos(pitch/2) * math.cos(yaw/2) + math.sin(roll/2) * math.sin(pitch/2) * math.sin(yaw/2)
+    return [qx, qy, qz, qw]
+
+# Convert rotation from quaternion to euler.
+def quaternion_to_euler(q):
+    (x, y, z, w) = (q[0], q[1], q[2], q[3])
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll = math.atan2(t0, t1)
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch = math.asin(t2)
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw = math.atan2(t3, t4)
+    return [yaw, pitch, roll]
